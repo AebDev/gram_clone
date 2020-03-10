1b60ee58def49728bd29436f0080bc973b7dd068
unknown <mohcine.aeb@gmail.com>
2020-03-10 22:01:28 +0100

    40m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+   <div class="row">
+       <div class="col-4"><img src="\img\index.jpg" class="rounded-circle m-5" alt="profile"></div>
+       <div class="col-8 mt-5">
+       <div><h1>freecodecamp</h1></div>
+       <div class="d-flex">
+           <div class="pr-3"><strong>153</strong> posts</div>
+           <div class="px-3"><strong>23k</strong> folowers</div>
+           <div class="pl-3"><strong>212</strong> folowing</div>
+       </div>
+       <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+           <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+       <div>
+       <a href="https://www.freecodecamp.org/">freecodecamp.org</a>
+       </div>
+   </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pr-2"><img src="\svg\logo.svg" style="width:25px" alt="logo"></div>
+                    <div class="pl-2" style="border-left:1px solid #000">Gram Clone</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
