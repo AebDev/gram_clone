0d154df223c8b5f2f2f8902fcc42d79ea2b24398
unknown <mohcine.aeb@gmail.com>
2020-03-10 20:50:50 +0100

    26m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    gram clone
                 </div>
 
                 <div class="links">
